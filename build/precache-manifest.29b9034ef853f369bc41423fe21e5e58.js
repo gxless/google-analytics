e23c03a2100682df49e5ca5c0042d7eeb3ee9642
gaollxll <hitgxless@gmail.com>
2020-09-12 17:13:51 -0400

    Add build folder

---
+++ build/precache-manifest.29b9034ef853f369bc41423fe21e5e58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20da49fb199d597986e09795ee99fe06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6961d769bf2438b9d008",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "def943199fe01e2134b6",
+    "url": "/static/js/2.4bc983f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4bc983f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6961d769bf2438b9d008",
+    "url": "/static/js/main.fdd5a6c8.chunk.js"
+  },
+  {
+    "revision": "c7854691b8d8c6939754",
+    "url": "/static/js/runtime-main.9af711a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
